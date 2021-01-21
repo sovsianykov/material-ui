eb73867aff45c400e204351955cd04856ab169fb
”sovsianykov” <swebproger@gmail.com>
2021-01-21 19:26:46 +0200

    first commit

---
+++ precache-manifest.21043e6d88c20fbb9bb19e5ca26d3806.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f294fb5b6463ea37b00d86a733e2cf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e9a70bb965c55983330",
+    "url": "/static/css/main.b1204bcd.chunk.css"
+  },
+  {
+    "revision": "210415b14b9c85a72291",
+    "url": "/static/js/2.7bbf2108.chunk.js"
+  },
+  {
+    "revision": "7069b205d702e72b0206de48781de38a",
+    "url": "/static/js/2.7bbf2108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e9a70bb965c55983330",
+    "url": "/static/js/main.1a90299f.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "4a83b3264065557cfd88faf5c0e4a47a",
+    "url": "/static/media/avatar.4a83b326.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/static/media/react.f370c1e0.png"
+  }
+]);
